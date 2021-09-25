ed486d2db5cb213c9b978906fbcb38221eac5c5f
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-09-25 20:01:47 +0900

    Create

--- src/main/typescript/index.ts
+++ src/main/typescript/index.ts
@@ -1,14 +1,68 @@
 
-exports.handler = async(event: any) => {
+import { CloudWatchLogs } from 'aws-sdk'
+
+exports.handler = (event: any) => {
+
+  // 抽出するログデータの最大件数
+  const OUTPUT_LIMIT = 5
+  // 何分前までを抽出対象期間とするか
+  const TIME_FROM_MIN = 10
+
   const message = JSON.parse(JSON.parse(JSON.stringify(event.Records[0].Sns.Message)))
 
-  console.log("========Records====================")
-  console.log(event.Records[0])
-  console.log("============================")
-  console.log("========SNS====================")
-  console.log(event.Records[0].Sns)
-  console.log("============================")
-  console.log("========message====================")
-  console.log(message)
-  console.log("============================")
+  const stateChangeTime = new Date(message['StateChangeTime'])
+
+  const timeTo   = stateChangeTime.setMinutes(stateChangeTime.getMinutes() + 1)
+  const unixTo   = Date.parse(new Date(timeTo).toUTCString()) / 1000
+  const timeFrom = stateChangeTime.setMinutes(stateChangeTime.getMinutes() - TIME_FROM_MIN)
+  const unixFrom = Date.parse(new Date(timeFrom).toUTCString()) / 1000
+
+  console.log('===============================')
+  console.log('stateChangeTime')
+  console.log(stateChangeTime)
+  console.log('===============================')
+  console.log('timeTo')
+  console.log(timeTo)
+  console.log(new Date(timeTo))
+  console.log('===============================')
+  console.log('unixTo')
+  console.log(unixTo)
+  console.log('===============================')
+  console.log('timeFrom')
+  console.log(timeFrom)
+  console.log(new Date(timeFrom))
+  console.log('===============================')
+  console.log('unixFrom')
+  console.log(unixFrom)
+  console.log('===============================')
+
+  const cloudWatchLogs = new CloudWatchLogs({ apiVersion: '2014-03-28' })
+
+  cloudWatchLogs.describeMetricFilters({
+    metricName:      message['Trigger']['MetricName'],
+    metricNamespace: message['Trigger']['Namespace']
+  }, (error, res) => {
+    if (error) { console.log(error) }
+    else {
+      console.log('==========================')
+      console.log('metricFilters')
+      console.log(res)
+      console.log(res.metricFilters![0].metricTransformations)
+
+      cloudWatchLogs.filterLogEvents({
+        logGroupName:  res.metricFilters![0].logGroupName!,
+        filterPattern: res.metricFilters![0].filterPattern!,
+        startTime:     timeFrom,
+        endTime:       timeTo,
+        limit:         OUTPUT_LIMIT
+      }, (error, res) => {
+        if (error) { console.log(error) }
+        else {
+          console.log('==========================')
+          console.log('filterLogEvents')
+          console.log(res)
+        }
+      })
+    }
+  })
 }
