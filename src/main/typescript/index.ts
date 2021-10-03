3f97a717753adebc5916be51db7eca573e8d1cac
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-10-03 16:58:33 +0900

    Fix Cloud Watch Alart to Slack

--- src/main/typescript/index.ts
+++ src/main/typescript/index.ts
@@ -3,54 +3,48 @@ import { CloudWatchLogs } from 'aws-sdk'
 
 import { WebClient } from '@slack/web-api'
 
-exports.handler = (event: any) => {
+import { slackMessage } from './slackMessage'
+
+exports.handler = (event: any, context: any, callback: any) => {
 
   // 抽出するログデータの最大件数
   const OUTPUT_LIMIT = 5
   // 何分前までを抽出対象期間とするか
   const TIME_FROM_MIN = 10
+  // 何分後までを抽出対象期間とするか
+  const TIME_TO_MAX = 1
+  // 何分前までのログを取得するか
+  const LOG_FILTER_TIME_FROM_MIN = 20
 
-  /**
-   * API Client for Slack
-   */
+  const TIME_ZONE_TOKYO = 'Asia/Tokyo'
+
+  // AWS Lambdaのリージョンを取得
+  const region = process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION
+
+  /** API Client for Slack */
   const web = new WebClient(process.env.SLACK_API_TOKEN)
 
+  /** API Client for CloudWatchLogs */
   const cloudWatchLogs = new CloudWatchLogs({ apiVersion: '2014-03-28' })
 
-  const message = JSON.parse(JSON.parse(JSON.stringify(event.Records[0].Sns.Message)))
+  /** SNSから送られてくる情報をJSONに変換 */
+  const snsMessage = JSON.parse(JSON.parse(JSON.stringify(event.Records[0].Sns.Message)))
 
-  const stateChangeTime = new Date(message['StateChangeTime'])
-
-  const timeTo   = stateChangeTime.setMinutes(stateChangeTime.getMinutes() + 1)
-  //const unixTo   = Date.parse(new Date(timeTo).toUTCString()) / 1000
-  const timeFrom = stateChangeTime.setMinutes(stateChangeTime.getMinutes() - TIME_FROM_MIN)
-  //const unixFrom = Date.parse(new Date(timeFrom).toUTCString()) / 1000
-
-  /*
-  console.log('===============================')
-  console.log('stateChangeTime')
-  console.log(stateChangeTime)
-  console.log('===============================')
-  console.log('timeTo')
-  console.log(timeTo)
-  console.log(new Date(timeTo))
-  console.log('===============================')
-  console.log('unixTo')
-  console.log(unixTo)
-  console.log('===============================')
-  console.log('timeFrom')
-  console.log(timeFrom)
-  console.log(new Date(timeFrom))
-  console.log('===============================')
-  console.log('unixFrom')
-  console.log(unixFrom)
-  console.log('===============================')
+  /**
+   * CloudWatch Alarmの状態変更時間を使用してCloudWatch Event、Filter用の時間を生成する
    */
+  const stateChangeTime    = new Date(snsMessage['StateChangeTime'])
+  const logFilterTime      = new Date(snsMessage['StateChangeTime'])
+  const logFilterEndTime   = logFilterTime.getTime()
+  const logFilterStartTime = logFilterTime.setMinutes(logFilterTime.getMinutes() - LOG_FILTER_TIME_FROM_MIN)
+  const endTime            = stateChangeTime.setMinutes(stateChangeTime.getMinutes() + TIME_TO_MAX)
+  const startTime          = stateChangeTime.setMinutes(stateChangeTime.getMinutes() - TIME_FROM_MIN)
 
+  /** MetricFilterの一覧を取得する */
   cloudWatchLogs.describeMetricFilters({
-    metricName:      message['Trigger']['MetricName'],
-    metricNamespace: message['Trigger']['Namespace']
-  }, (error, res) => {
+    metricName:      snsMessage['Trigger']['MetricName'],
+    metricNamespace: snsMessage['Trigger']['Namespace']
+  }, (error, describeMetricFiltersResponse) => {
     if (error) { console.log(error) }
     else {
       //console.log('==========================')
@@ -58,21 +52,62 @@ exports.handler = (event: any) => {
       //console.log(res)
       //console.log(res.metricFilters![0].metricTransformations)
 
+      const metricFilters: CloudWatchLogs.MetricFilters = describeMetricFiltersResponse.metricFilters!
+      const logGroupName:  string = metricFilters[0].logGroupName!
+
       cloudWatchLogs.filterLogEvents({
-        logGroupName:  res.metricFilters![0].logGroupName!,
-        filterPattern: res.metricFilters![0].filterPattern!,
-        startTime:     timeFrom,
-        endTime:       timeTo,
+        logGroupName:  logGroupName,
+        filterPattern: metricFilters[0].filterPattern!,
+        startTime:     startTime,
+        endTime:       endTime,
         limit:         OUTPUT_LIMIT
-      }, (error, res) => {
+      }, (error, filterLogEventsResponse) => {
         if (error) { console.log(error) }
         else {
-          console.log('==========================')
-          console.log('filterLogEvents')
-          console.log(JSON.parse(JSON.stringify(res.events![0].message)))
-          console.log(JSON.parse(JSON.stringify(res.events![0].message)))
+          const events = filterLogEventsResponse.events!.slice(-1)[0]
+          //console.log('==========================')
+          //console.log('filterLogEvents')
+          //console.log(filterLogEventsResponse)
+          //console.log(JSON.parse(JSON.stringify(events.message)))
 
-          web.chat.postMessage()
+          const logStreamName: string = events.logStreamName!
+          const trackingId:    string = JSON.parse(events.message!).tags[0]
+
+          /** CloudWatch LogsへのURLを生成する */
+          const cloudWatchLogsUrl = [
+            `https://${region}.console.aws.amazon.com/`,
+            `cloudwatch/home?region=${region}`,
+            '#logsV2:log-groups/log-group/',
+            `${logGroupName.replace(/\//g, '$252F')}/`,
+            'log-events/',
+            `${logStreamName}`,
+            '$3FfilterPattern$3D',
+            `${trackingId}`,
+            `$26start$3D${logFilterStartTime}`,
+            `$26end$3D${logFilterEndTime}`
+          ].join('')
+
+          // https://ap-northeast-1.console.aws.amazon.com/cloudwatch/home?region=ap-northeast-1#alarmsV2:alarm/stg-error-alarm?
+          /** CloudWatch AlarmへのURLを生成する */
+          const cloudWatchAlarmUrl = [
+            `https://${region}.console.aws.amazon.com/`,
+            `cloudwatch/home?region=${region}`,
+            '#alarmsV2:alarm/',
+            `${snsMessage['AlarmName']}?`
+          ].join('')
+
+          const eventMessage = JSON.parse(events.message!)
+
+          const status     = metricFilters[0].filterPattern!
+          const count      = metricFilters[0].metricTransformations![0].metricValue
+          const message    = eventMessage.message
+          const loggerName = eventMessage.logger_name
+          const threadName = eventMessage.thread_name
+          const timestamp  = new Date(eventMessage['@timestamp']).toLocaleString('ja-JP', { timeZone: TIME_ZONE_TOKYO })
+          const params     = slackMessage(status, process.env.ECS_CLUSTER_NAME!, count, cloudWatchLogsUrl, cloudWatchAlarmUrl, message, loggerName, threadName, timestamp)
+          web.chat.postMessage(params).then(
+            callback(null, 'Validation test succeeded')
+          ).catch(console.error)
         }
       })
     }
