21ac12cdefd14995dba02c289c96b90d7e7156c2
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-10-03 16:58:16 +0900

    Fix slackMessage

--- src/main/typescript/slackMessage.ts
+++ src/main/typescript/slackMessage.ts
@@ -2,11 +2,15 @@
 import { ChatPostMessageArguments } from '@slack/web-api'
 
 export function slackMessage(
-  status:           string, // CloudWatch Alarmの状態
-  appName:          string, // CloudWatch Alarmが発生したアプリ名 (ECS Cluster名)
-  count:            string, // CloudWatch Alarmの発生回数
-  cloudWatchLogUrl: string, // CloudWatch LogsのURL
-  log:              string  // CloudWatch メトリクスフィルターで取得したログ内容
+  status:             string, // CloudWatch Alarmの状態
+  appName:            string, // CloudWatch Alarmが発生したアプリ名 (ECS Cluster名)
+  count:              string, // CloudWatch Alarmの発生回数
+  cloudWatchLogsUrl:  string, // CloudWatch LogsのURL
+  cloudWatchAlarmUrl: string, // CloudWatch AlarmのURL
+  message:            string, // アプリケーションログのメッセージ
+  loggerName:         string, // アプリケーションログの発生箇所
+  threadName:         string, // アプリケーションログの発生時のスレッド
+  timestamp:          string  // アプリケーションログの発生時間
 ): ChatPostMessageArguments {
 
   return {
@@ -36,21 +40,59 @@ export function slackMessage(
         "type": "section",
         "text": {
           "type": "mrkdwn",
-          "text": `<${cloudWatchLogUrl}|CloudWatchへ>`
+          "text": `<${cloudWatchLogsUrl}|CloudWatch Logsで確認する>`
         }
-      }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `<${cloudWatchAlarmUrl}|CloudWatch Alarmで確認する>`
+        }
+      },
+      {
+        "type": "divider"
+      },
     ],
     "attachments": [
       {
-        "color": "#dc143c",
+        "color": `${status === 'ERROR' ? '#dc143c' : '#ffa500'}`,
         "blocks": [
           {
             "type": "section",
             "text": {
               "type": "mrkdwn",
-              "text": '```' + `${JSON.parse(JSON.stringify(log))}` + '```'
+              "text": `${status}発生箇所: ${loggerName}`
             }
-          }
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": `${status}発生時のスレッド: ${threadName}`
+            }
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": `アプリケーションログの発生時間: ${timestamp}`
+            }
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": `アプリケーションログの${status}メッセージ`
+            }
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": '```' + `${message}` + '```'
+            }
+          },
         ]
       }
     ]
